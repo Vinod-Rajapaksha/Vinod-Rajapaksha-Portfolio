8e41b5bf1c125f63fbad999c9a15d75df001592f
Vinod-Rajapaksha <165497570+Vinod-Rajapaksha@users.noreply.github.com>
2025-11-20 01:06:05 +0530

    bug fix : in mobile view 3d canvas issue

--- src/components/sections/Tech.tsx
+++ src/components/sections/Tech.tsx
@@ -1,11 +1,29 @@
+import { useEffect, useState } from "react";
 import BallCanvas from "../canvas/Ball";
 import SectionWrapper from "../../hooks/section-wrapper";
 import { technologies } from "../../data/technologies";
 
 const Tech = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth < 640);
+    };
+
+    handleResize(); 
+    window.addEventListener("resize", handleResize);
+
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  const visibleTechnologies = isMobile
+    ? technologies.slice(0, 6) 
+    : technologies;            
+
   return (
     <div className="flex flex-row flex-wrap justify-center gap-10">
-      {technologies.map((technology) => (
+      {visibleTechnologies.map((technology) => (
         <div className="h-28 w-28" key={technology.name}>
           <BallCanvas icon={technology.icon} />
         </div>
