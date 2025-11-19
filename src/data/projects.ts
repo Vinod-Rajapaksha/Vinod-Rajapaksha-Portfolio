8739720bfdddbfb57f8a38a4d995c05963117031
Vinod-Rajapaksha <165497570+Vinod-Rajapaksha@users.noreply.github.com>
2025-11-19 12:48:31 +0530

    update : project data

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,11 +1,11 @@
 export const projects = [
   {
-    title: " Employee-Management-System",
+    title: " Employee Management System",
     description: "👨‍💼 Employee Management System is a full-stack web application built with the MERN stack (MongoDB, Express.js, React.js, Node.js). It enables administrators to efficiently manage employee records with full CRUD functionality, a modern React-based UI, a secure and scalable Node.js + Express backend, and a MongoDB-powered RESTful API.",
     tech: ["React", "Express.js", "Node.js","MongoDB"],
     demoUrl: "https://www.linkedin.com/posts/vinod-rajapaksha_mern-fullstackdeveloper-reactjs-activity-7347814142122016768-1cX-?utm_source=share&utm_medium=member_desktop&rcm=ACoAAEWTQhABlCL5huTssw4TKtfEz7DeRMoYJmo",
     codeUrl: "https://github.com/Vinod-Rajapaksha/Employee-Management-System",
-    image: "",
+    image: "/FoodOrderingSystem.png",
   },
   {
     title: "AI-Powered Job Recommendation System",
@@ -13,22 +13,22 @@ export const projects = [
     tech: ["Angular", "Spring Boot", "PostgreSQL","Tailwind CSS"],
     demoUrl: "https://www.linkedin.com/in/vinod-rajapaksha/",
     codeUrl: "https://github.com/Vinod-Rajapaksha/AI-powered-Job-Recommendation-System",
-    image: "",
+    image: "/FoodOrderingSystem.png",
   },
   {
-    title: "Tourism-and-Travel-Management-System",
+    title: "Tourism and Travel Management System",
     description: "A full-stack web application for managing tourism and travel operations. Customers can browse, book, and pay for tour packages online, while staff, managers, and tour guides manage bookings, packages, and customer interactions. Built with React, Spring Boot, SQL Server, and Bootstrap.",
     tech: ["React", "Spring Boot", "Microsoft SQL Server"],
     demoUrl: "https://github.com/Vinod-Rajapaksha/Tourism-and-Travel-Management-System",
     codeUrl: "https://github.com/Vinod-Rajapaksha/Tourism-and-Travel-Management-System",
-    image: "",
+    image: "/FoodOrderingSystem.png",
   },
   {
-    title: "Food-Ordering-System",
+    title: "Food Ordering System",
     description: "This project is designed to make the process of browsing menus, placing orders, and managing food services seamless for customers and administrators.",
     tech: ["JSP", "Java", "Tomcat", "MySQL"],
     demoUrl: "https://github.com/Vinod-Rajapaksha/Food-Ordering-System",
     codeUrl: "https://github.com/Vinod-Rajapaksha/Food-Ordering-System",
-    image: "",
+    image: "/FoodOrderingSystem.png",
   },
 ];
